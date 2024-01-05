04c310facbaad6e7b771ffc4f23e14128f31ea63
mateusz-wojnar <mwojnar87@gmail.com>
2024-01-05 17:23:37 +0100

    Implemented navbar and footer components

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,6 @@
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
+
 const MarketingLayout = ({
     children
 }: {
@@ -5,11 +8,13 @@ const MarketingLayout = ({
 }) => {
     return (
         <div className="h-full bg-slate-100">
-            {/* Navbar */}
+            <Navbar>
+
+            </Navbar>
             <main className="pt-40 pb-20 bg-slate-100">
                 {children}
             </main>
-            {/* Footer */}
+            <Footer></Footer>
            
         </div>
     )
