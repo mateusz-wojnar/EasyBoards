df66c7d994f1a8400c4698de2adf7cc3e878f976
mateusz-wojnar <mwojnar87@gmail.com>
2024-01-18 23:03:13 +0100

    Implemented dynamic titles based on currently active org

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,18 @@
+import { startCase } from "lodash"
+import { auth } from "@clerk/nextjs"
+
 import { OrgControl } from "./_components/org-control"
+
+// function to generate title based on org user is in
+export async function generateMetadata() {
+    const {orgSlug} = auth()
+
+    return {
+        title: startCase(orgSlug || "organization")
+    }
+        
+    
+}
 
 const OrganizationIdLayout = ({
     children
